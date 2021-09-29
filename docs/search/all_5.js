0aa04e94120026f2aaf43f76b68f00edae474745
Docs Robot <robot@example.com>
2021-09-29 11:42:40 +0000

    Committing documentation changes

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['enabled_0',['enabled',['../class_u_c1701___s_o_l_d_e_r_e_d.html#a76c729eb282aee2ce187a0e2053dc23d',1,'UC1701_SOLDERED']]],
-  ['endwrite_1',['endWrite',['../class_adafruit___g_f_x.html#ab9be8787f3683151e86c1f3b5c8685d6',1,'Adafruit_GFX::endWrite()'],['../class_u_c1701___s_o_l_d_e_r_e_d.html#a6f4c48f4ceff96f6f1b9c8d7affb03b4',1,'UC1701_SOLDERED::endWrite()']]]
+  ['endwrite_1',['endWrite',['../class_adafruit___g_f_x.html#ab9be8787f3683151e86c1f3b5c8685d6',1,'Adafruit_GFX']]]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['endwrite_0',['endWrite',['../class_adafruit___g_f_x.html#ab9be8787f3683151e86c1f3b5c8685d6',1,'Adafruit_GFX::endWrite()'],['../class_u_c1701___s_o_l_d_e_r_e_d.html#a6f4c48f4ceff96f6f1b9c8d7affb03b4',1,'UC1701_SOLDERED::endWrite()']]]
+  ['endwrite_0',['endWrite',['../class_adafruit___g_f_x.html#ab9be8787f3683151e86c1f3b5c8685d6',1,'Adafruit_GFX']]]
 ];
