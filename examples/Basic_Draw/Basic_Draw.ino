ba8103d2c8557ef728d42f3d16947f49973ad5df
Main Push Robot <robot@example.com>
2022-02-24 10:42:43 +0000

    Committing dev changes

--- examples/Basic_Draw/Basic_Draw.ino
+++ examples/Basic_Draw/Basic_Draw.ino
@@ -16,21 +16,21 @@ UC1701_SOLDERED display;
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(115200);   //Start serial communication with PC using 115200 baudrate
 
     // Initialize the display with the pins used
     display.begin(21, 22, 13, 14, 15, 16); // MOSI, CLK, CS, CD, RST
-
+    
     // All Adafruit GFX functions available
 
-    display.drawRect(20, 10, 50, 50, 1);
-    display.fillCircle(50, 50, 10, 1);
-    display.drawLine(0, 0, 100, 100, 1);
+    display.drawRect(20, 10, 50, 50, 1);    //Draw rectangle with coordinates (x0,y0,x1,y1,color)
+    display.fillCircle(50, 50, 10, 1);      //Draw flled circle with coordinates (x0,y0,radius,color)
+    display.drawLine(0, 0, 100, 100, 1);    //Draw line with start and end point (x0,y0,x1,y1,color)
 
-    display.setCursor(40, 20);
+    display.setCursor(40, 20);              //Set cursor location for text writinf (x,y)
     display.print("Hello World!");
 
-    display.display();
+    display.display();                      //Display buffer on screen
 }
 
 void loop()
